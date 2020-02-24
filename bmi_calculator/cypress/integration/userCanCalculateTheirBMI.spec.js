8c081d46557a181b7b20be71ccdd61496aeedaab
Emma_Thalen <thlalen.emma@gmail.com>
2020-02-24 13:14:22 +0100

    taking out the folder in a folder

--- bmi_calculator/cypress/integration/userCanCalculateTheirBMI.spec.js
+++
@@ -1,9 +0,0 @@
-describe('BMI Calculator', () => {
-    it('Calculates BMI in metric', () => {
-        cy.visit('/');
-        cy.get('input#weight').type(90);
-        cy.get('input#height').type(190);
-        cy.get('button#calculate').click();
-        cy.get('p#bmi-message').should('contain', 'You are Normal with a BMI of 24.93')
-    })
-})
